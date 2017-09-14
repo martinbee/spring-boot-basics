635192fce89abb63baf4b9927c5dccc586f9fa22
Martin Bee <martbee88@gmail.com>
2017-09-14 07:15:27 -0500

    DI: review assignment setup

--- sections/3/src/main/java/guru/springframework/controllers/GetterInjectedController.java
+++ sections/3/src/main/java/guru/springframework/controllers/GetterInjectedController.java
@@ -1,20 +1,23 @@
 package guru.springframework.controllers;
 
 import guru.springframework.services.GreetingService;
-import guru.springframework.services.GreetingServiceImpl;
-import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
 
+/**
+ * Created by jt on 5/24/17.
+ */
 @Controller
-public class SetterInjectedController {
-    @Autowired
+public class GetterInjectedController {
     private GreetingService greetingService;
 
     public String sayHello(){
         return greetingService.sayGreeting();
     }
 
-    public void setGreetingService(GreetingService greetingService) {
+    @Autowired
+    public void setGreetingService(@Qualifier("getterGreetingService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 }
